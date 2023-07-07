90717a09f4645c46a41fd44045ef41137048e1b5
amarouane-ABDELHAK <am0089@uah.edu>
2023-07-07 15:09:39 -0500

    Add test

---
+++ integration_test/test_stac_api.py
@@ -0,0 +1,21 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def test_stac_url_returns_200():
+    endpoint = os.getenv("STAC_URL")
+    stac_path_prefix = os.getenv("VEDA_STAC_PATH_PREFIX")
+    url = f"{endpoint.rstrip('/')}/{stac_path_prefix}/health"
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_raster_url_returns_200():
+    endpoint = os.getenv("RASTER_URL")
+    raster_path_prefix = os.getenv("VEDA_RASTER_PATH_PREFIX")
+    url = f"{endpoint.rstrip('/')}/{raster_path_prefix}/health"
+    response = requests.get(url)
+    assert response.status_code == 200
